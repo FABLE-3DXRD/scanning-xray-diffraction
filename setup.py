3cc820eaa79ce861dcf0473032cd2d3dee8cd60b
Axel <axel@hlf-74.solid.lth.se>
2021-04-08 10:22:49 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="s3dxrd",
-    version="0.0.4",
+    version="0.0.5",
     author="Axel Henningsson",
     author_email="nilsaxelhenningsson@gmail.com",
     description="Tools for intragranular strain estimation with s3dxrd data.",
