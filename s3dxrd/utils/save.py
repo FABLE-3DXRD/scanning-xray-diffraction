e3fa483cb0dbe7753d3cec749210e7ff1a414f30
Axel <axel@hlf-74.solid.lth.se>
2021-05-14 09:42:08 +0200

    added save function for mesh data to vtu paraview

--- s3dxrd/utils/save.py
+++ s3dxrd/utils/save.py
@@ -1,6 +1,34 @@
 from pyevtk.hl import gridToVTK
+from pyevtk.hl import pointsToVTK
 import numpy as np
 import pickle
+
+def mesh2voxels(file, mesh, zpos, values, names):
+    """Save arrays as fields that are paraview compatible (vtk files).
+
+    Args:
+        file (:obj:`string`): Asolute path to save the output at ending with desired filename.
+        mesh (:obj:`list` of :obj:`numpy array`): Series of 2d x-y element mesh coordinate arrays.
+        zpos (:obj:`list` of :obj:`float`): z-cordinates of each of the meshes in ´´´mesh´´´.
+        values (:obj:`list` of :obj:`list` of :obj:`numpy array`): For each mesh this list contains
+            a list for each tensor component and each mesh element. I.e values[i][j][k] gives the 
+            value associated to the k:th element in the mesh[i] for the tensor component j.
+        names (:obj:`list` of :obj:`string`): Names of the tensor component arrays refered to by 
+            paraview. These will also be the default titles in paraview.
+    """
+    cellData = { name:[] for name in names }
+    xg,yg,zg = [],[],[]
+    for i,m in enumerate(mesh):
+        for j,(x,y) in enumerate( zip( np.mean(m[:,0::2],axis=1), np.mean(m[:,1::2],axis=1) )):
+            xg.append(x)
+            yg.append(y)
+            zg.append(zpos[i])
+            for k,name in enumerate(names):
+                cellData[name].append(values[i][k][j])
+    for k,name in enumerate(names):
+        cellData[name] = np.array(cellData[name])
+
+    pointsToVTK(file, np.array(xg), np.array(yg), np.array(zg), cellData)
 
 def as_vtk_voxel_volume(file, arrays, names):
     """Save arrays as fields that are paraview compatible (vtk files).
